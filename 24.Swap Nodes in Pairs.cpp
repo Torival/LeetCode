8911595a89333e0fce5e44cff53dc30e9e115143
Torival <260780887@qq.com>
2015-11-04 14:03:58 +0800

    easy question

---
+++ 24.Swap Nodes in Pairs.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(!head || !head->next) return head;
+        ListNode *p, *s;
+        int temp;
+        p = head;
+        s = p->next;
+        while(s) {
+			temp = p->val;
+			p->val = s->val;
+			s->val = temp;
+			s = s->next;
+			if(s) {
+				s = s->next;
+				p = p->next->next;
+			}
+		}	
+		return head;
+    }
+};
---
+++ 242.Valid Anagram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int lens = s.length();
+        int lent = t.length();
+        if((lens != lent)) return false;
+        if(!lens && !lent) return true;
+		int a[128] = {0};
+		for(int i=0; i<lens; i++) {
+			a[s[i]]++;
+		}
+		for(int i=0; i<lent; i++) {
+			a[t[i]]--;
+		}
+		for(int i=0;i<128; i++) {
+			if(a[i]) return false;
+		}
+		return true;
+    }
+};
