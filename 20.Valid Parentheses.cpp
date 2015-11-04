09e8e4f12771191dd4a784fd2e7844817d1ecf04
Torival <260780887@qq.com>
2015-11-04 14:19:18 +0800

    Update 20.Valid Parentheses.cpp

--- 20.Valid Parentheses.cpp
+++ 20.Valid Parentheses.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     bool isValid(string s) {
-		 int top = 0;
-		 int k;
-		 char a[s.length()];
-		 for(int i=0; i<s.length(); i++) {
-		 	if(!top) a[top++] = s[i];
-		 	else {
-		 		k = a[top-1]-s[i];
-				if(k==1 || k==-1 || k==2 || k==-2) top--;
-		 		else a[top++] = s[i];
-			 }
+	int top = 0;
+	int k;
+	char a[s.length()];
+	for(int i=0; i<s.length(); i++) {
+		if(!top) a[top++] = s[i];
+		else {
+			k = a[top-1]-s[i];
+			if(k==1 || k==-1 || k==2 || k==-2) top--;
+			else a[top++] = s[i];
 		 }
-		 return top>0 ? false:true;
+	 }
+	return top>0 ? false:true;
     }
 };
