bbffb8b3eaa93268fccde982d10ba1c969c713c7
Torival <260780887@qq.com>
2015-11-06 22:19:28 +0800

    three Pointer

---
+++ 206.Reverse Linked List.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(!head || !head->next) return head;
+        ListNode *p = head, *s = p->next, *t = s->next;
+        p->next = NULL;
+		while(t) {
+        	s->next = p;
+        	p = s;
+        	s = t;
+        	t = t->next;
+		}
+		s->next = p;
+		return s;
+    }
+};
