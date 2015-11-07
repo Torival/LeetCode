fe4121fd89271ea6334d5e9cad5bb026df678380
Torival <260780887@qq.com>
2015-11-07 18:52:11 +0800

    long long

--- 7.Reverse Integer.cpp
+++ 7.Reverse Integer.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int reverse(int x) {
-        int num = 0;
+        long long num = 0;
         int x1 = abs(x);
-        
-        for(int i=1; i<=x1; i*=10) {
-            //判断是否正负溢出，小溢出变为负数，大溢出变为正数 
-            if(num >= INT_MAX/10 && (10*num + x1/i%10<0 || (INT_MAX-(10*num + x1/i%10))/10))
-                return 0;
+
+        for(long long i=1; i<=x1; i*=10) {
             num = 10*num + x1/i%10;
-            if(x1/i < 10) break;
+            if(num > INT_MAX) return 0;
         }
         return x > 0 ? num :-num;
     }
