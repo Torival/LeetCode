645272e988b614ffd1eeaa364122f037124fc296
Torival <260780887@qq.com>
2015-11-10 00:30:41 +0800

    Medium

--- 92.Reverse Linked List II.cpp
+++ 92.Reverse Linked List II.cpp
@@ -10,22 +10,24 @@ class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int m, int n) {
         if(!head || m==n) return head;
-        ListNode* p = head ,*s;
+        ListNode* p = head ,*s = head;
         n -= m;
-        while(m) {
+        while(m-1) {
             s = p, m--;
             p = p->next;
         }
-        if(!p || !p->next) return head;
-        ListNode *t1 = p, *t2 = t1->next, *t3 = t2->next;
-    
-        while(n) {
+        ListNode *t1 = p, *t2 = t1->next, *t3 = t2->next;	
+		while(n) {
             t2->next = t1;
             t1 = t2, t2 = t3, n--;
-            t3 = t3->next;
+            if(t3) t3 = t3->next;
         }
-        s->next->next = t3;
-        s->next = t2;
-        return head;       
+        if(p == head){ 
+            s->next = t2;
+            return t1;
+        }
+        s->next->next = t2;
+        s->next = t1;
+        return head;   
     }
 };
