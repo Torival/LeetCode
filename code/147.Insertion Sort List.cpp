041d75b8059f1ab6bf9fb9e657dabc7aee5675e4
Torival <260780887@qq.com>
2015-11-26 23:34:24 +0800

    Insertion sort

---
+++ code/147.Insertion Sort List.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        if(!head || !head->next) return head;
+        ListNode* psort = new ListNode(0);
+        psort->next = head;
+        ListNode *ptail = head, *pre;
+        head = head->next, ptail->next = NULL;
+        while(head) {
+            pre = psort;
+            ptail = pre->next;
+            while(ptail && ptail->val < head->val) {
+                pre = pre->next;
+                ptail = ptail->next;
+            }
+            pre->next = head;
+            head = head->next;
+            pre->next->next = ptail;
+        }
+        return psort->next;
+    }
+};
+
