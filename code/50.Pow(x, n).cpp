9833c577b1ffd780063356eff61a986eb8c3f238
Torival <260780887@qq.com>
2015-11-27 13:13:23 +0800

    divide

---
+++ code/50.Pow(x, n).cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(x == 0) return 0;
+        
+        if(n == 0) return 1;
+        else if(n == -1) return 1/x;
+        else if(n == 1) return x;
+        else if(n & 1) {
+            double temp = myPow(x,(n-1)/2);
+            return temp*temp*x;
+        }
+        else {
+            double temp = myPow(x,n/2);
+            return temp*temp;
+        }
+    }
+};
