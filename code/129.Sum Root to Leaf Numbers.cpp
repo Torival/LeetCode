18674cb569da1dadae51b79625a1c0d595679f00
Torival <260780887@qq.com>
2015-12-02 14:23:32 +0800

    Tree

---
+++ code/129.Sum Root to Leaf Numbers.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) {
+        if(!root) return 0;
+        return sumNumbers(root, 0);
+    }
+    int sumNumbers(TreeNode* root, long long num) {
+        if(!root) return 0;
+        long long sum = 10*num + root->val;
+        if(root->left || root->right) {
+            sum = sumNumbers(root->left, num*10 + root->val);
+            sum += sumNumbers(root->right, num*10 + root->val);
+        }
+        return sum;
+    }
+};
