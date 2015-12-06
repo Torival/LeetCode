98945ab5683eeff64910332374aaa554ca812020
Torival <260780887@qq.com>
2015-12-06 22:27:20 +0800

    Tree

---
+++ code/113.Path Sum II.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> pathSum(TreeNode* root, int sum) {
+        vector<vector<int>> vec;
+        vector<int> v;
+        pathSum(root, sum, vec, v, 0);
+        return vec;
+    }
+    
+    void pathSum(TreeNode* root, int sum, vector<vector<int>> &vec, vector<int> v, int total) { 
+        if(!root) return ;
+        v.push_back(root->val);
+        if(!root->left && !root->right && sum == total + root->val) vec.push_back(v);
+        pathSum(root->left, sum, vec, v, total + root->val);
+        pathSum(root->right, sum, vec, v, total + root->val);
+    }
+};
