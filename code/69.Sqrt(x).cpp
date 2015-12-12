3fc7b64822dcab42de5f7e8b323a640e0b8fd052
Torival <260780887@qq.com>
2015-12-12 21:58:12 +0800

    BS

--- code/69.Sqrt(x).cpp
+++ code/69.Sqrt(x).cpp
@@ -9,7 +9,6 @@ public:
             if(a*a == x) return a;
             else if(a*a > x) high = a - 1;
             else low = a + 1;
-            
             a = (low+high)/2;
         }
         return a;
