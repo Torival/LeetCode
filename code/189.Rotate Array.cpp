e9dc94e8641f54021c66b9ff958397e946fa9fb3
Torival <260780887@qq.com>
2015-12-26 20:25:37 +0800

    Array

---
+++ code/189.Rotate Array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        if(!(k %= nums.size())) return ;
+        
+        int index = 0, count = 0;
+        int temp = nums[0];
+        
+        for(int i=0; i<nums.size(); i++) {
+            if(count == index) {
+                temp = nums[++count];
+                index++;
+            }
+            index = (index+k)%nums.size();
+            swap(temp, nums[index]);
+        }
+    }
+}; 
