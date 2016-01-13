68483b290e58e324e02e75b768eb6eda917ed4a9
Torival <260780887@qq.com>
2016-01-13 22:24:36 +0800

    Update 111.Minimum Depth of Binary Tree.cpp

--- code/111.Minimum Depth of Binary Tree.cpp
+++ code/111.Minimum Depth of Binary Tree.cpp
@@ -6,20 +6,24 @@
  *     TreeNode *right;
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
- *结点三种状态：叶子，单支结点，双支结点 
+ *缁撶偣涓夌鐘舵�侊細鍙跺瓙锛屽崟鏀粨鐐癸紝鍙屾敮缁撶偣 
  */
 class Solution {
 public:
     int minDepth(TreeNode* root) {
         if (!root) return 0;
+        
         int depthl = 0;
         int depthr = 0;
-        if(!root->left && !root->right)
-            return 1;
+        
+        if(!root->left && !root->right) return 1;
+        
         depthl = minDepth(root->left);
         depthr = minDepth(root->right);
+        
         if(!depthl) return depthr+1;
         if(!depthr) return depthl+1;
+        
         return depthl < depthr ? depthl+1 : depthr+1;
     }
 };
