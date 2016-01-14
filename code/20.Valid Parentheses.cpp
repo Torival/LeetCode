e84f296d9ad68bbdcdca4f9820573b98e8cc2ea1
Torival <260780887@qq.com>
2016-01-14 20:49:54 +0800

    Update 20.Valid Parentheses.cpp

--- code/20.Valid Parentheses.cpp
+++ code/20.Valid Parentheses.cpp
@@ -6,7 +6,8 @@ public:
         char a[s.length()];
 
         for(int i=0; i<s.length(); i++) {
-            if(!top) a[top++] = s[i];
+            if(!top) 
+                a[top++] = s[i];
             else {
                 k = a[top-1]-s[i];
                 if(k==1 || k==-1 || k==2 || k==-2) top--;
